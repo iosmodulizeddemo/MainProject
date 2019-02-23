817d2c6ca812fb264d56732fc1878f9f08279bdb
Snowman <479992945@qq.com>
2019-02-23 11:20:57 +0800

    init

---
+++ MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by Panda on 2019/2/23.
+//  Copyright © 2019年 Panda.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by Panda on 2019/2/23.
+//  Copyright © 2019年 Panda.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
